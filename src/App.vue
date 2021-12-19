e13fe2b32d328dd17a6617f133589f697735cc20
Forever-Dreamer <2926204175@qq.com>
2021-12-20 00:52:04 +0800

    添加了管理员界面（未完成）

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,15 @@ import CountDown from './components/TianYan/CountDown'
 import Gameresult1 from './components/TianYan/Gameresult1'
 import Gameresult2 from './components/TianYan/Gameresult2'
 
+import AccuracyCount from './components/TianYan/Administrator/AccuracyCount'
+import Admin from './components/TianYan/Administrator/Admin'
+import AdminSidebar from './components/TianYan/Administrator/AdminSidebar'
+import Check from './components/TianYan/Administrator/Check'
+import DealFeedback from './components/TianYan/Administrator/DealFeedback'
+import IdentifyCount from './components/TianYan/Administrator/IdentifyCount'
+import Knowledge from './components/TianYan/Administrator/Knowledge'
+import TrainData from './components/TianYan/Administrator/TrainData'
+
 export default {
   name: "App",
   components: {
@@ -49,7 +58,16 @@ export default {
     Feedback,
     CountDown,
     Gameresult1,
-    Gameresult2
+    Gameresult2,
+
+    AccuracyCount,
+    Admin,
+    AdminSidebar,
+    Check,
+    DealFeedback,
+    IdentifyCount,
+    Knowledge,
+    TrainData,
   }
 }
 </script>
---
+++ src/components/TianYan/Administrator/Admin.vue
@@ -0,0 +1,171 @@
+<template>
+    <el-container>
+        <Header/>
+        <el-container>
+            <AdminSidebar/>
+            <el-main>
+                <div class="usertext">
+                    <h3>用户管理</h3>
+                </div>
+                <div class="search">
+                <el-input v-model="input" placeholder="请输入内容" style="width:170px"></el-input>
+                <el-button type="primary" icon="el-icon-search" style="width:90px">搜索</el-button>
+                <el-button type="primary" icon="el-icon-plus" style="width:90px" id="add">添加</el-button>
+                </div>
+
+                <div class="table">
+                    <el-table
+                        :data="tableData"
+                        border
+                        style="width:100%"
+                        height="450"
+                        >
+                        <el-table-column
+                            prop="id"
+                            label="ID"
+                            width="180">
+                        </el-table-column>
+                        <el-table-column
+                            prop="name"
+                            label="昵称"
+                            width="180">
+                        </el-table-column>
+                        <el-table-column
+                            prop="email"
+                            label="邮箱"
+                            width="180">
+                        </el-table-column>
+                        <el-table-column
+                            prop="score"
+                            label="游戏积分"
+                            width="140">
+                        </el-table-column>
+                        <el-table-column
+                            prop="or"
+                            label="是否为管理员"
+                            width="140">
+                        </el-table-column>
+                        <el-table-column
+                            label="操作"
+                            width="200">
+                            <template slot-scope="scope">
+                                <el-button @click="handleClick(scope.row)" icon="el-icon-edit" size="small" type="primary">编辑</el-button>
+                                <el-button icon="el-icon-delete" size="small" type="danger">删除</el-button>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </div>
+            </el-main>
+        </el-container>
+    </el-container>
+
+</template>
+
+<script>
+import Header from './Header'
+import AdminSidebar from './AdminSidebar'
+export default {
+    name: 'Admin',
+    components:{Header,AdminSidebar},
+    methods:{
+        handleOpen(key,keyPath){
+            console.log(key,keyPath);
+        },
+        handleClose(key,keyPath){
+            console.log(key,keyPath);
+        },
+        handleClick(row){
+            console.log(row);
+        }
+    },
+    data(){
+        return {
+            input:'',
+            tableData:[{
+                id:'2018',
+                name:'1',
+                email:'1',
+                score:'1',
+                or:'是'
+            },{
+                id:'2018',
+                name:'1',
+                email:'1',
+                score:'1',
+                or:'是'
+            },{
+                id:'2018',
+                name:'1',
+                email:'1',
+                score:'1',
+                or:'是'
+            },{
+                id:'2018',
+                name:'1',
+                email:'1',
+                score:'1',
+                or:'是'
+            },{
+                id:'2018',
+                name:'1',
+                email:'1',
+                score:'1',
+                or:'是'
+            },{
+                id:'2018',
+                name:'1',
+                email:'1',
+                score:'1',
+                or:'是'
+            },{
+                id:'2018',
+                name:'1',
+                email:'1',
+                score:'1',
+                or:'是'
+            },{
+                id:'2018',
+                name:'1',
+                email:'1',
+                score:'1',
+                or:'是'
+            },{
+                id:'2018',
+                name:'1',
+                email:'1',
+                score:'1',
+                or:'是'
+            },{
+                id:'2018',
+                name:'1',
+                email:'1',
+                score:'1',
+                or:'是'
+            }]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.usertext{
+    position:absolute;
+    left:300px;
+    top:100px;
+}
+.search{
+    position:absolute;
+    left:300px;
+    top:170px;
+}
+.search #add{
+    position:absolute;
+    left:935px;
+    top:0px;
+}
+.table{
+    position:absolute;
+    left:300px;
+    top:250px;
+}
+</style>
